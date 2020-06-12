7198bc41316936444cb35619c5ec444c9585f6ec
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-12 20:12:35 +0100

    setup header regex

--- src/directives/decode.js
+++ src/directives/decode.js
@@ -1,5 +1,19 @@
+const rules = [
+  [/^(?:######)\s*(.+?)[ \t]*$/gm, '<h6>$1</h6>'],
+  [/^(?:#####)\s*(.+?)[ \t]*$/gm, '<h5>$1</h5>'],
+  [/^(?:####)\s*(.+?)[ \t]*$/gm, '<h4>$1</h4>'],
+  [/^(?:###)\s*(.+?)[ \t]*$/gm, '<h3>$1</h3>'],
+  [/^(?:##)\s*(.+?)[ \t]*$/gm, '<h2>$1</h2>'],
+  [/^(?:#)\s*(.+?)[ \t]*$/gm, '<h1>$1</h1>']
+]
 export default {
-  bind: el => {
-    console.log(el.textContent)
+  bind(el) {
+    let content = el.textContent
+    rules.forEach(([rule, template]) => {
+
+      content = content.replace(rule, template);
+      console.log(content)
+    })
+    el.innerHTML = content
   }
 }
