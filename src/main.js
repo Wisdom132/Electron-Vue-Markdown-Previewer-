cd95e2259d753b6fc4187e3d904e944448d71f54
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-12 21:43:17 +0100

    using package to test

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-import decode from '@/directives/decode'
-// import VueDirectiveMarkdown from 'vue-directive-markdown';
-// Vue.directive('decode', VueDirectiveMarkdown)
+// import decode from '@/directives/decode'
+// Vue.directive('decode', decode)
 
+import VueDirectiveMarkdown from 'vue-directive-markdown'
+Vue.directive('decode', VueDirectiveMarkdown)
 
-Vue.directive("decode", decode)
 Vue.config.productionTip = false
 
 new Vue({
